c8b8639514d7a06f2b0eeb91ad1f0dffeb9eecb7
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-01-22 00:17:24 +0600

    created get products slice

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,13 +1,37 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
-  value: 0,
+  products: [],
+  isLoading: false,
+  error: null,
 };
+
+const BASE_URL = "http://localhost:3000/products";
+
+export const fetchProducts = createAsyncThunk(
+  "products/fetchProducts",
+  async () => {
+    const res = await axios.get(BASE_URL);
+    return res.data;
+  },
+);
 
 export const productsSlice = createSlice({
   name: "products",
   initialState,
-  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchProducts.pending, (state) => {
+      ((state.isLoading = true), (state.error = null));
+    });
+    builder.addCase(fetchProducts.fulfilled, (state, action) => {
+      ((state.isLoading = false), (state.products = action.payload));
+    });
+    builder.addCase(fetchProducts.rejected, (state, action) => {
+      ((state.isLoading = false),
+        (state.error = action.error.message || "Failed to fetch data"));
+    });
+  },
 });
 
 export default productsSlice.reducer;
